ec991a6fc2836a886968363c8359a0446a48bc41
Andy <andyqulam@gmail.com>
2025-01-25 12:35:49 -0800

    sawblade animation

--- ScenesAndScripts/sawblade.gd
+++ ScenesAndScripts/sawblade.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var animation = $AnimatedSprite2D
+
+func _animation():
+	animation.play("default")
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.damage(0)
+		
