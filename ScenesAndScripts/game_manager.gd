555375e6eb058268083d4d98c17233f8d5397f81
metal-maniacs <dlarobotics@outlook.com>
2025-01-25 13:57:08 -0800

    crossbow and gamemanager work

---
+++ ScenesAndScripts/game_manager.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+
+@export var phase1: Array[Node2D]
+
+func _ready():
+	timer.start()
+	
+
+
+
+func _on_timer_timeout():
+	for device in phase1:
+		if device.enabled == false:
+			device.enable()
+		elif device.enabled == true:
+			device.disable()
+		else:
+			printerr("could not determine device state")
