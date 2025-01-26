2429b1fd96aac7e2f75527aa6d233f4988f8cc4f
Spunboing <diamondjellybean457@gmail.com>
2025-01-26 13:57:25 -0800

    added knights and phase timer to main level

--- ScenesAndScripts/game_manager.gd
+++ ScenesAndScripts/game_manager.gd
@@ -8,31 +8,50 @@ extends Node2D
 @onready var r3arr = get_tree().get_nodes_in_group("sr3")
 @onready var r4arr = get_tree().get_nodes_in_group("sr4")
 
+#Knights
+@onready var knightarr = get_tree().get_nodes_in_group("knight")
+
 #Timers
-@onready var timer: Timer = $Timer
+@onready var phase_timer: Timer = $phase_time
 @onready var spikecyctime: Timer = $spikecyctime
+@onready var knight_cycle_time: Timer = $knight_cycle_time
 
-
+@export var phase = 1
 @export var phase1: Array[Node2D]
-
-var nextspike = 10
+@export var nextspike = 10
 
 func _ready():
 	spike_spawns()
+	knight_spawns()
 
-	timer.start()
+
+
+func knight_index():
+	return int(randf_range(0,knightarr.size()-1))
+	
+func knight_spawns():
+		for i in phase:
+			var ind = knight_index()
+			if !(knightarr[ind].gogo):
+				print("hi")
+				knightarr[ind].enable()
+	
 
 func spike_spawns():
 	for i in r1arr:
 		i.enable()
-	await get_tree().create_timer(nextspike).timeout
+	await get_tree().create_timer(nextspike/phase).timeout
 	for i in r2arr:
 		i.enable()
-	await get_tree().create_timer(nextspike).timeout
+	await get_tree().create_timer(nextspike/phase).timeout
 	for i in r3arr:
 		i.enable()
-	await get_tree().create_timer(nextspike).timeout
+	await get_tree().create_timer(nextspike/phase).timeout
 	for i in r4arr:
 		i.enable()
-	if nextspike > 5:
-		nextspike -= 6
+	spike_spawns()
+
+
+func _on_phase_time_timeout() -> void:
+	if phase < 3:
+		phase += 1
--- ScenesAndScripts/knight.gd
+++ ScenesAndScripts/knight.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @export var speed = 75
 @export var chargespeed = 150
 @export var activate = false
+@export var gogo = false
 
-@onready var player: CharacterBody2D = $"../player"
+@onready var player: CharacterBody2D = $"../../player"
 @onready var knight: CharacterBody2D = $"."
 @onready var timer: Timer = $Timer
 @onready var pos = position
@@ -14,6 +15,7 @@ var dir = 1
 var attack = false
 
 func enable():
+	gogo = true
 	activate = true
 
 func check_dir():
@@ -24,29 +26,30 @@ func check_dir():
 	need_to_check_direction = false
 
 func _physics_process(delta):
-	if !attack:
-		if position.x <= player.position.x:
-			scale.x = 1
-		else:
-			scale.x = -1
+	if gogo:
+		if !attack:
+			if position.x <= player.position.x:
+				scale.x = 1
+			else:
+				scale.x = -1
 
-	if activate:
-		if position.y >= player.position.y -3 && position.y <= player.position.y + 3:
-			attack = true
-			print("CHARRRGE")
-		elif position.y < player.position.y:
-			if !attack:
-				position.y+=delta*speed
-		elif position.y > player.position.y:
-			if !attack:
-				position.y-=delta*speed
+		if activate:
+			if position.y >= player.position.y -3 && position.y <= player.position.y + 3:
+				attack = true
+				print("CHARRRGE")
+			elif position.y < player.position.y:
+				if !attack:
+					position.y+=delta*speed
+			elif position.y > player.position.y:
+				if !attack:
+					position.y-=delta*speed
 
-		if attack:
-			position.x+=delta*chargespeed*dir
-			if timer.is_stopped():
-				timer.start()
-			if need_to_check_direction:
-				check_dir()
+			if attack:
+				position.x+=delta*chargespeed*dir
+				if timer.is_stopped():
+					timer.start()
+				if need_to_check_direction:
+					check_dir()
 
 func _on_timer_timeout() -> void:
 	position = pos
