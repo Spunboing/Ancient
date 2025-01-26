b81dad2cc5b84ac7d113af17a575f4d4eefaad26
Spunboing <diamondjellybean457@gmail.com>
2025-01-26 14:54:56 -0800

    bats added into main level

--- ScenesAndScripts/batt.gd
+++ ScenesAndScripts/batt.gd
@@ -1,22 +1,46 @@
 extends CharacterBody2D
 
-@onready var player: CharacterBody2D = $"../player"
+@onready var player: CharacterBody2D = $"../../player"
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var timer: Timer = $Timer
+@onready var pos = position
+
 @export var speed = 50
 var player_pos
 var target_pos
+var active = false
+var goaway = false
 
+func enable():
+	timer.start()
+	active = true
+	collision_shape_2d.disabled = false
+
+func disable():
+	goaway = true
+	active = false
+	collision_shape_2d.disabled = true
+	await get_tree().create_timer(3).timeout
+	goaway = false
+	position = pos
 
 func _process(delta):
-	
-	player_pos = player.position
-	target_pos = (player_pos - position).normalized()
-	
-	if position.distance_to(player_pos) > 3:
-		velocity = target_pos*speed
-		move_and_slide()
-		look_at(player_pos)
+	if active:
+		player_pos = player.position
+		target_pos = (player_pos - position).normalized()
+		
+		if position.distance_to(player_pos) > 3:
+			velocity = target_pos*speed
+			move_and_slide()
+			look_at(player_pos)
 
+func _physics_process(delta):
+	if goaway:
+		position.y += 1000*delta
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.damage(0)
+
+func _on_timer_timeout() -> void:
+	disable()
--- ScenesAndScripts/game_manager.gd
+++ ScenesAndScripts/game_manager.gd
@@ -11,10 +11,15 @@ extends Node2D
 #Knights
 @onready var knightarr = get_tree().get_nodes_in_group("knight")
 
+#bat array
+@onready var batarr = get_tree().get_nodes_in_group("bat")
+
 #Timers
 @onready var phase_timer: Timer = $phase_time
 @onready var spikecyctime: Timer = $spikecyctime
 @onready var knight_cycle_time: Timer = $knight_cycle_time
+@onready var bat_time: Timer = $bat_time
+
 
 @export var phase = 1
 @export var phase1: Array[Node2D]
@@ -23,9 +28,21 @@ extends Node2D
 func _ready():
 	spike_spawns()
 	knight_spawns()
+	bat_spawns()
 
 
+#Handles all bat traps
+func bat_spawns():
+	if phase == 2:
+		batarr[int(randf_range(0,batarr.size()-1))].enable()
+	elif phase == 3:
+		batarr[0].enable()
+		batarr[1].enable()
+	
+	bat_time.start()
 
+
+#Handles all knight traps
 func knight_index():
 	return int(randf_range(0,knightarr.size()-1))
 	
@@ -36,7 +53,7 @@ func knight_spawns():
 				print("hi")
 				knightarr[ind].enable()
 	
-
+#Handles all spike traps
 func spike_spawns():
 	for i in r1arr:
 		i.enable()
@@ -51,7 +68,13 @@ func spike_spawns():
 		i.enable()
 	spike_spawns()
 
-
+#Moves to next phase every minute until phase 3 is reached
 func _on_phase_time_timeout() -> void:
 	if phase < 3:
 		phase += 1
+	if phase == 2:
+		bat_spawns()
+
+
+func _on_bat_time_timeout() -> void:
+	pass # Replace with function body.
