f3ef59c525b59167c00c1cafef6152a38ce708b3
Spunboing <diamondjellybean457@gmail.com>
2025-01-26 13:02:29 -0800

    finished spike cycles in main level

--- ScenesAndScripts/game_manager.gd
+++ ScenesAndScripts/game_manager.gd
@@ -4,13 +4,18 @@ extends Node2D
 @onready var vert: Node = $"../Spike Things/Spike area 2ds/vert"
 @onready var horizontal: Node = $"../Spike Things/Spike area 2ds/horizontal"
 @onready var r1arr = get_tree().get_nodes_in_group("sr1")
+@onready var r2arr = get_tree().get_nodes_in_group("sr2")
+@onready var r3arr = get_tree().get_nodes_in_group("sr3")
+@onready var r4arr = get_tree().get_nodes_in_group("sr4")
 
-
+#Timers
 @onready var timer: Timer = $Timer
+@onready var spikecyctime: Timer = $spikecyctime
+
 
 @export var phase1: Array[Node2D]
 
-var nextspike = true
+var nextspike = 10
 
 func _ready():
 	spike_spawns()
@@ -20,13 +25,14 @@ func _ready():
 func spike_spawns():
 	for i in r1arr:
 		i.enable()
-
-
-func _on_timer_timeout():
-	for device in phase1:
-		if device.enabled == false:
-			device.enable()
-		elif device.enabled == true:
-			device.disable()
-		else:
-			printerr("could not determine device state")
+	await get_tree().create_timer(nextspike).timeout
+	for i in r2arr:
+		i.enable()
+	await get_tree().create_timer(nextspike).timeout
+	for i in r3arr:
+		i.enable()
+	await get_tree().create_timer(nextspike).timeout
+	for i in r4arr:
+		i.enable()
+	if nextspike > 5:
+		nextspike -= 6
