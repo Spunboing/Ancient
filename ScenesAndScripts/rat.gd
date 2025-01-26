c744d7bb2ac23c2b0e624353cf05f962905d910e
metal-maniacs <dlarobotics@outlook.com>
2025-01-26 12:05:01 -0800

    added rat spawn in period

--- ScenesAndScripts/rat.gd
+++ ScenesAndScripts/rat.gd
@@ -4,6 +4,7 @@ extends AnimatedSprite2D
 @export var SPEED: float
 
 var running = false
+var canDamage = false
 
 @onready var startPos = global_position
 
@@ -15,6 +16,11 @@ func enable():
 	visible = true
 	enabled = true
 	running = true
+	canDamage = false
+	modulate = Color.from_hsv(0, 1, 1)
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate", Color.WHITE, 1)
+	tween.tween_callback(enableDamage)
 	
 func disable():
 	print("disabling rat")
@@ -29,14 +35,15 @@ func _process(delta):
 	query.exclude = [self]
 	var result = spaceState.intersect_ray(query)
 	
-	if running:
+	if running and canDamage:
 		position += Vector2.RIGHT.rotated(rotation) * delta * SPEED
 	if result:
-		if result.collider.is_in_group("player"):
+		if result.collider.is_in_group("player") and canDamage:
 			result.collider.damage(0)
 		else:
 			var setAngle = (Vector2(cos(rotation), sin(rotation)).bounce(result.normal).angle())
 			rotation = setAngle
-			if result.collider.is_in_group("player"):
-				result.collider.damage(0)
 			
+
+func enableDamage():
+	canDamage = true
